9a6b1d457706e2adfbdc650c632dc1f3b1c9e913
Brandon Lewis <blewis@adorapos.com>
2023-06-28 15:16:54 -0700

    updated file names

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 import { DiscordClient } from './models/DiscordClient.js';
 import dotenv from 'dotenv';
 dotenv.config();
- 
 const client = new DiscordClient(process.env.DISCORD_TOKEN, process.env.CLIENT_ID, process.env.GUILD_ID);
-
 client.initialize();
